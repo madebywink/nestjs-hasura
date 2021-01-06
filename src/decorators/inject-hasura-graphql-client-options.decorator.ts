943fcf84f3c9e0a4f333242eb984367b27953429
Matt Cain <matt@madebywink.com>
2021-01-05 22:55:44 -0500

    test removal

--- src/decorators/inject-hasura-graphql-client-options.decorator.ts
+++ src/decorators/inject-hasura-graphql-client-options.decorator.ts
@@ -1,21 +1,9 @@
 import { Inject } from "@nestjs/common";
-import {
-  HASURA_SDK_OPTIONS_INJECT,
-  NAMED_HASURA_SDK_OPTIONS_INJECT,
-} from "../hasura.tokens";
+import { HASURA_SDK_OPTIONS_INJECT } from "../hasura.tokens";
 
 /**
- * Inject the GraphQL Request Client instance options. If multiple instances are being used,
- * the `name` parameter is used to distinguish between them
- *
- * @param name optional name of Hasura instance, if using multiple
+ * Inject the GraphQL Request Client instance options.
  */
-export function InjectHasuraGraphQLClientOptions(
-  name?: string
-): () => ParameterDecorator {
-  if (!name) {
-    return () => Inject(HASURA_SDK_OPTIONS_INJECT);
-  }
-
-  return () => Inject(NAMED_HASURA_SDK_OPTIONS_INJECT(name));
+export function InjectHasuraGraphQLClientOptions(): () => ParameterDecorator {
+  return () => Inject(HASURA_SDK_OPTIONS_INJECT);
 }
